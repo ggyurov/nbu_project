1528149670a7d8f6c027f68db36f967fefe749d5
Petar Petrov <petarpetrovt@gmail.com>
2017-10-21 17:36:39 +0300

    Improve project structure and database connection string

--- src/ARSFD.Web/Startup.cs
+++ src/ARSFD.Web/Startup.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using ARSFD.Web.Data;
+﻿using ARSFD.Web.Data;
 using ARSFD.Web.Models;
 using ARSFD.Web.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ARSFD.Web
 {
