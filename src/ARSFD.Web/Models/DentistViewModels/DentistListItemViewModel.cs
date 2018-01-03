60b17b8d23a4477492ce5471fa76c4b42f480da0
Valentin Nikolov <vnikolov@david.bg>
2018-01-03 14:38:30 +0200

    Add stub information for searching dentists

---
+++ src/ARSFD.Web/Models/DentistViewModels/DentistListItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ARSFD.Web.Models.DentistViewModels
+{
+	public class DentistListItemViewModel
+	{
+		public string Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string City { get; set; }
+
+		public string Type { get; set; }
+
+		public double Rating { get; set; }
+	}
+}
