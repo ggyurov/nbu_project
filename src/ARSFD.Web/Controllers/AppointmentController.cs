a1109b1a601e04b84d6758ccaee7693da8bd5f90
Petar Petrov <petarpetrovt@gmail.com>
2018-01-04 13:51:41 +0200

    Improve site main menu

---
+++ src/ARSFD.Web/Controllers/AppointmentController.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ARSFD.Services;
+using ARSFD.Web.Views.Appointment;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ARSFD.Web.Controllers
+{
+	[Route("appointment")]
+	public class AppointmentController: Controller
+	{
+		private readonly IUserService _userService;
+
+		public AppointmentController(IUserService userService)
+		{
+			_userService = userService;
+		}
+
+		[HttpGet]
+		[Route("")]
+		public async Task<IActionResult> Index(
+			[FromQuery(Name = "Name")] string name,
+			[FromQuery(Name = "City")] string city,
+			[FromQuery(Name = "Type")] string type,
+			[FromQuery(Name = "Rating")] double? rating,
+			CancellationToken cancellationToken = default)
+		{
+			var model = new AppointmentIndexViewModel();
+
+			return View(model);
+		}
+
+		[Route("{id}")]
+		public async Task<IActionResult> Single(
+			[FromRoute(Name = "id")] string id,
+			CancellationToken cancellationToken = default)
+		{
+
+			return View();
+		}
+	}
+}
---
+++ src/ARSFD.Web/Views/Appointment/AppointmentIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ARSFD.Web.Views.Appointment
+{
+	public class AppointmentIndexViewModel
+	{
+	}
+}
