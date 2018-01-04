b1c62975159134d89947ebc9a31cd57bca7794a8
Petar Petrov <petarpetrovt@gmail.com>
2018-01-04 15:24:03 +0200

    Add calendar view, controller, menu item

---
+++ src/ARSFD.Web/Controllers/CalendarController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ARSFD.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ARSFD.Web.Controllers
+{
+	[Route("calendar")]
+	public class CalendarController: Controller
+	{
+		private readonly IUserService _userService;
+
+		public CalendarController(IUserService userService)
+		{
+			_userService = userService;
+		}
+
+		[HttpGet]
+		[Route("")]
+		public async Task<IActionResult> Index(
+			[FromQuery(Name = "Name")] string name,
+			[FromQuery(Name = "City")] string city,
+			[FromQuery(Name = "Type")] string type,
+			[FromQuery(Name = "Rating")] double? rating,
+			CancellationToken cancellationToken = default)
+		{
+			return View();
+		}
+
+		[Route("{id}")]
+		public async Task<IActionResult> Single(
+			[FromRoute(Name = "id")] string id,
+			CancellationToken cancellationToken = default)
+		{
+
+			return View();
+		}
+	}
+}
