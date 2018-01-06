c1b859f4422bbef5b272d3263d1c32fa2dd69d61
Petar Petrov <petarpetrovt@gmail.com>
2018-01-06 14:25:36 +0200

    Add patient page view

---
+++ src/ARSFD.Web/Models/PatientViewModels/PatientViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ARSFD.Web.Models.CommentViewModels;
+
+namespace ARSFD.Web.Models.PatientViewModels
+{
+	public class PatientViewModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public double Rating { get; set; }
+
+		public CommentViewModel[] Comments { get; set; }
+
+		public bool IsBlackListed { get; set; }
+
+		public bool IsRated { get; set; }
+
+		public int RateValue { get; set; }
+
+		[Required(ErrorMessage = "Полето `{0}` е задължително.")]
+		[Display(Name = "Мнение")]
+		public string CommentText { get; set; }
+	}
+}
